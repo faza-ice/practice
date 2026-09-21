c5eb1748cb5d0d173fee9ad05e257547cfa94a56
Pavel <pivnevp41@gmail.com>
2026-09-21 12:29:10 +0600

    поправка регистра

--- src/widgets/header/ui/Header/Header.jsx
+++ src/widgets/header/ui/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = ()=>{
         <div className={styles.header}>
             <div className={styles.logo}>
                 <img src={imag} alt="" />
-                <div className={styles.name}><Title className={styles.title}>AANTHEE</Title><Text className={styles.text}><span className={styles.textMedium}>федеральная сеть</span></Text></div>
+                <div className={styles.name}><Title className={styles.title}>AANTHEE</Title><Text className={styles.text}><span className={styles.textMedium}>ФЕДЕРАЛЬНАЯ СЕТЬ</span></Text></div>
             </div>
             <nav className={styles.nav}>
                <Text><a href="" className={styles.textSemiBold}>Инструкторы</a></Text>
@@ -21,7 +21,7 @@ const Header = ()=>{
             </nav>
             <div className={styles.tech}>
                 <Title className={styles.title}><span className={styles.titleBold}>+7 (800) 555-35-35</span></Title>
-                <Button option='red' className={styles.button}>запись онлайн</Button>
+                <Button option='red' className={styles.button}>ЗАПИСЬ ОНЛАЙН</Button>
             </div>
         </div>
     )
